934471fafd549c46096a3db1dfdbd9afec1f5afa
Luke C Hartman <luke@lukecarlhartman.com>
2025-04-11 16:41:49 -0700

    on local dev site header home link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import styles from './Header.module.css';
+import React from "react"
+import styles from "./Header.module.css"
 
 export const Header: React.FC = () => {
-  return (
-    <header className={`container container--wide ${styles.header}`} role="banner">
-      <div className={styles.siteTitle}>The Gazette Star</div>
-    </header>
-  );
-}; 
+	return (
+		<header className={`container container--wide ${styles.header}`} role='banner'>
+			<div className={styles.siteTitle}>
+				The Gazette Star
+				<span>
+					{process.env.NODE_ENV === "development" && (
+						<a href='http://localhost:3000' className={styles.homeLink}>
+							Home
+						</a>
+					)}
+				</span>
+			</div>
+			{/* would like to add a link to the base url here that is only visible on local development */}
+		</header>
+	)
+}
