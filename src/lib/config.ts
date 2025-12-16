2602febe9174959bca39dc61beca3e96db0af781
Luke C Hartman <luke@lukecarlhartman.com>
2025-12-16 13:15:31 -0800

    update config.ts (minor cleanup)

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -61,16 +61,16 @@ export interface ArticleConfig {
  */
 export const defaultConfig: ArticleConfig = {
   author: {
-    name: "(default) Jim Phelps",
+    name: "Jim Phelps",
     bio: {
-      personal: "(default) Jim Phelps is a science reporter for The Gazette Star. His coverage of energy and the environment has appeared in the Dallas Morning News, The Atlantic and Newsweek. A Colorado native and life-long Broncos fan, he began his career at the Denver Post, where he was part of a team that won a Pulitzer Prize for their story about the pollution of popular hot springs in Aspen. He graduated with a journalism degree from Vanderbilt University where he served as the editor-in-chief of the student newspaper. His simple pleasures in life include hiking with his wife and two sons and the smell of barbecue on the lakefront after surviving a cold winter.",
-      basic: "(default) Jim Phelps is a science reporter for The Gazette Star. His coverage of energy and the environment has appeared in the Dallas Morning News, The Atlantic and Newsweek. He began his career at the Denver Post, where he was part of a team that won a Pulitzer Prize for their story about the pollution of popular hot springs in Aspen. He graduated with a journalism degree from Vanderbilt University and served as editor-in-chief of the student newspaper."
+      personal: "Jim Phelps is a science reporter for The Gazette Star. His coverage of energy and the environment has appeared in the Dallas Morning News, The Atlantic and Newsweek. A Colorado native and life-long Broncos fan, he began his career at the Denver Post, where he was part of a team that won a Pulitzer Prize for their story about the pollution of popular hot springs in Aspen. He graduated with a journalism degree from Vanderbilt University where he served as the editor-in-chief of the student newspaper. His simple pleasures in life include hiking with his wife and two sons and the smell of barbecue on the lakefront after surviving a cold winter.",
+      basic: "Jim Phelps is a science reporter for The Gazette Star. His coverage of energy and the environment has appeared in the Dallas Morning News, The Atlantic and Newsweek. He began his career at the Denver Post, where he was part of a team that won a Pulitzer Prize for their story about the pollution of popular hot springs in Aspen. He graduated with a journalism degree from Vanderbilt University and served as editor-in-chief of the student newspaper."
     },
     image: {
       src: "/images/author-image.jpg",
-      alt: "(default) Author Image"
+      alt: "Author Image"
     }
   },
-  pubdate: "(default) Aug. 6, 2019",
-  siteName: "(default) The Gazette Star"
+  pubdate: "Aug. 6, 2019",
+  siteName: "The Gazette Star"
 }; 
